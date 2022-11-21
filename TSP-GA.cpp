46098a831ba5deafd454408599aa6f2fc99cae80
Thiloshon Nagarajah <thiloshon@gmail.com>
2022-11-20 19:34:39 -0800

    Add Openmp for mutation

--- TSP-GA.cpp
+++ TSP-GA.cpp
@@ -1,56 +1,59 @@
 // C++ implementation of the above approach
+#include <algorithm>
 #include <bits/stdc++.h>
 #include <limits.h>
+#include <iterator> // for std::begin, std::end
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <omp.h>
+
 using namespace std;
- 
+
 // Number of cities in TSP
-#define V 5
- 
-// Names of the cities
-#define GENES ABCDE
- 
-// Starting Node Value
-#define START 0
- 
+#define V 48
+
 // Initial population size for the algorithm
-#define POP_SIZE 10
- 
+#define POP_SIZE 12
+
+#define gen_threshold 300
+
+
+int best_value = INT_MAX;
+
 // Structure of a GNOME
 // string defines the path traversed
 // by the salesman while the fitness value
 // of the path is stored in an integer
- 
 struct individual {
-    string gnome;
+    vector<int> gnome;
     int fitness;
 };
- 
+
 // Function to return a random number
 // from start and end
-int rand_num(int start, int end)
-{
+int rand_num(int start, int end) {
     int r = end - start;
     int rnum = start + rand() % r;
     return rnum;
 }
- 
+
 // Function to check if the character
 // has already occurred in the string
-bool repeat(string s, char ch)
-{
+bool repeat(string s, char ch) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == ch)
             return true;
     }
     return false;
 }
- 
+
 // Function to return a mutated GNOME
 // Mutated GNOME is a string
 // with a random interchange
 // of two genes to create variation in species
-string mutatedGene(string gnome)
-{
+vector<int> mutatedGene(vector<int> gnome) {
     while (true) {
         int r = rand_num(1, V);
         int r1 = rand_num(1, V);
@@ -63,140 +66,199 @@ string mutatedGene(string gnome)
     }
     return gnome;
 }
- 
+
 // Function to return a valid GNOME string
 // required to create the population
-string create_gnome()
-{
-    string gnome = "0";
-    while (true) {
-        if (gnome.size() == V) {
-            gnome += gnome[0];
+vector<int> create_gnome() {
+    // TODO: Change characters to String Numbers (SHON)
+    vector<int> old_gnome;
+
+    for (int h = 0; h < V + 1; h++) {
+
+        if (h == V) {
+            old_gnome.push_back(0);
             break;
         }
-        int temp = rand_num(1, V);
-        if (!repeat(gnome, (char)(temp + 48)))
-            gnome += (char)(temp + 48);
+        old_gnome.push_back(h);
     }
-    return gnome;
+
+    auto rng = std::default_random_engine{};
+    std::shuffle(std::begin(old_gnome), std::end(old_gnome), rng);
+
+    return old_gnome;
 }
- 
+
 // Function to return the fitness value of a gnome.
 // The fitness value is the path length
 // of the path represented by the GNOME.
-int cal_fitness(string gnome)
-{
-    int map[V][V] = { { 0, 2, INT_MAX, 12, 5 },
-                    { 2, 0, 4, 8, INT_MAX },
-                    { INT_MAX, 4, 0, 3, 3 },
-                    { 12, 8, 3, 0, 10 },
-                    { 5, INT_MAX, 3, 10, 0 } };
+int cal_fitness(vector<int> gnome, int map[V][V]) {
     int f = 0;
-    for (int i = 0; i < gnome.size() - 1; i++) {
-        if (map[gnome[i] - 48][gnome[i + 1] - 48] == INT_MAX)
+    for (int i = 0; i < V; i++) {
+        if (map[gnome[i]][gnome[i + 1]] == INT_MAX)
             return INT_MAX;
-        f += map[gnome[i] - 48][gnome[i + 1] - 48];
+        f += map[gnome[i]][gnome[i + 1]];
+    }
+
+
+    if (f < best_value){
+#pragma omp critical
+        best_value = f;
     }
     return f;
 }
- 
+
 // Function to return the updated value
 // of the cooling element.
-int cooldown(int temp)
-{
-    return (90 * temp) / 100;
+int cooldown(int temp) {
+    // TODO: Evaluate need for Temp (SHON)
+    return (99 * temp) / 100;
 }
- 
+
 // Comparator for GNOME struct.
 bool lessthan(struct individual t1,
-            struct individual t2)
-{
+              struct individual t2) {
     return t1.fitness < t2.fitness;
 }
- 
+
+string get_string_vector(vector<int> arr) {
+    stringstream ss;
+    copy(arr.begin(), arr.end(), ostream_iterator<int>(ss, "->"));
+    string s = ss.str();
+    s = s.substr(0, s.length() - 1);
+
+    return s;
+}
+
 // Utility function for TSP problem.
-void TSPUtil(int map[V][V])
-{
+void TSPUtil(int map[V][V]) {
+
+
+
     // Generation Number
     int gen = 1;
-    // Number of Gene Iterations
-    int gen_thres = 5;
- 
+
     vector<struct individual> population;
     struct individual temp;
- 
+
+
+
     // Populating the GNOME pool.
     for (int i = 0; i < POP_SIZE; i++) {
         temp.gnome = create_gnome();
-        temp.fitness = cal_fitness(temp.gnome);
+        temp.fitness = cal_fitness(temp.gnome, map);
         population.push_back(temp);
     }
- 
-    cout << "\nInitial population: " << endl
-        << "GNOME     FITNESS VALUE\n";
-    for (int i = 0; i < POP_SIZE; i++)
-        cout << population[i].gnome << " "
-            << population[i].fitness << endl;
-    cout << "\n";
- 
-    bool found = false;
+
+
+//    cout << "\nInitial population: " << endl
+//         << "GNOME	 FITNESS VALUE\n";
+//    for (int i = 0; i < POP_SIZE; i++)
+//        cout << get_string_vector(population[i].gnome) << " "
+//             << population[i].fitness << endl;
+//    cout << "\n";
+
     int temperature = 10000;
- 
+
     // Iteration to perform
     // population crossing and gene mutation.
-    while (temperature > 1000 && gen <= gen_thres) {
+    while (temperature > 10 && gen <= gen_threshold) {
         sort(population.begin(), population.end(), lessthan);
-        cout << "\nCurrent temp: " << temperature << "\n";
         vector<struct individual> new_population;
- 
-        for (int i = 0; i < POP_SIZE; i++) {
-            struct individual p1 = population[i];
- 
-            while (true) {
-                string new_g = mutatedGene(p1.gnome);
-                struct individual new_gnome;
-                new_gnome.gnome = new_g;
-                new_gnome.fitness = cal_fitness(new_gnome.gnome);
- 
-                if (new_gnome.fitness <= population[i].fitness) {
-                    new_population.push_back(new_gnome);
-                    break;
-                }
-                else {
- 
-                    // Accepting the rejected children at
-                    // a possible probability above threshold.
-                    float prob = pow(2.7,
-                                    -1 * ((float)(new_gnome.fitness
-                                                - population[i].fitness)
-                                        / temperature));
-                    if (prob > 0.5) {
+
+
+        // CROSSOVER
+//        int crossover_point = 1 + (rand() % (V - 1));
+//        string first_half = population[0].gnome.substr(0, crossover_point);
+//        string second_half = "";
+//        for (int i = 0; i < population[1].gnome.size(); i++) {
+//            if (first_half.find(population[1].gnome[i]) == std::string::npos) {
+//                second_half += population[1].gnome[i];
+//            }
+//        }
+//        string newgenome = first_half + second_half + "0";
+//        population[POP_SIZE].gnome = newgenome;
+//        population[POP_SIZE].fitness = cal_fitness(population[POP_SIZE].gnome, map);
+
+        //        //Add fittest offspring to population
+
+        // MUTATION
+        #pragma omp parallel num_threads(4) default(none) shared(population, map, temperature, gen, new_population)
+        {
+            int id = omp_get_thread_num();
+            //printf("%d\n", gen);
+            #pragma omp for
+            for (int i = 0; i < POP_SIZE; i++) {
+                struct individual p1 = population[i];
+
+                int tempp = 0;
+
+                while (tempp < POP_SIZE * 50) {
+                    tempp += 1;
+
+                    //Do mutation under a random probability
+                    //                if (rn.nextInt()%7 < 5) {
+                    //                    demo.mutation();
+                    //                }
+
+                    vector<int> new_g = mutatedGene(p1.gnome);
+                    struct individual new_gnome;
+                    new_gnome.gnome = new_g;
+                    new_gnome.fitness = cal_fitness(new_gnome.gnome, map);
+
+                    if (new_gnome.fitness <= population[i].fitness) {
+                        #pragma omp critical
                         new_population.push_back(new_gnome);
                         break;
+                    } else {
+                        // Accepting the rejected children at
+                        // a possible probability above threshold.
+                        float prob = pow(2.7,
+                                         -1 * ((float) (new_gnome.fitness
+                                                        - population[i].fitness)
+                                               / temperature));
+                        if (prob > 0.5) {
+                            #pragma omp critical
+                            new_population.push_back(new_gnome);
+                            break;
+                        }
                     }
                 }
             }
+
         }
- 
+
+
         temperature = cooldown(temperature);
         population = new_population;
-        cout << "Generation " << gen << " \n";
-        cout << "GNOME     FITNESS VALUE\n";
- 
-        for (int i = 0; i < POP_SIZE; i++)
-            cout << population[i].gnome << " "
-                << population[i].fitness << endl;
+//        cout << "\nGeneration " << gen << " \n";
+//        cout << "\nCurrent temp: " << temperature << "\n";
+//        cout << "GNOME	 FITNESS VALUE\n";
+//
+//        for (int i = 0; i < POP_SIZE; i++)
+//            cout << get_string_vector(population[i].gnome) << " "
+//                 << population[i].fitness << endl;
         gen++;
     }
+    cout << "\nBest Value " << best_value << " \n";
 }
- 
-int main()
-{
- 
-    int map[V][V] = { { 0, 2, INT_MAX, 12, 5 },
-                    { 2, 0, 4, 8, INT_MAX },
-                    { INT_MAX, 4, 0, 3, 3 },
-                    { 12, 8, 3, 0, 10 },
-                    { 5, INT_MAX, 3, 10, 0 } };
-    TSPUtil(map);
+
+int main() {
+
+    int distances[V][V];
+    int x, y;
+    ifstream in("C:\\Users\\Thiloshon\\CLionProjects\\untitled\\att48_d.txt");
+
+    if (!in) {
+        cout << "Cannot open file.\n";
+        return 0;
+    }
+
+    for (y = 0; y < V; y++) {
+        for (x = 0; x < V; x++) {
+            in >> distances[x][y];
+        }
+    }
+    in.close();
+
+    TSPUtil(distances);
 }
